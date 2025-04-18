7fd1a87515380836db0b0138a3a8a814aff8eac8
Louis Maxwell <louismaxwell@knights.ucf.edu>
2025-04-18 05:02:16 -0400

    🐛 Fix page_config call order in Subject Editor

--- pages/2_Subject_Editor.py
+++ pages/2_Subject_Editor.py
@@ -1,6 +1,5 @@
-import streamlit as st
 st.set_page_config(page_title="Dashboard", layout="centered")  # MUST be first!
-
+import streamlit as st
 import pandas as pd
 import calendar
 from datetime import datetime, timedelta
